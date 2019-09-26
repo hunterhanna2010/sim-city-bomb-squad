a8a904b5147c8c36fe0fcda2a6a579658c04ee8a
hunterhann2010 <josh.m.hunter@gmail.com>
2019-09-26 14:35:36 -0700

    html css layout

--- bomb.js
+++ bomb.js
@@ -1,1 +1,24 @@
 console.log('loaded!');
+
+//VARIABLE DECLARATIONS
+const STARTING_TIME = 30;
+var remainingTime = 0;
+var gameOver = false;
+var countdown = null;
+var delay = null;
+
+
+var wireState = {
+    blue: false,
+    green: false,
+    red: false,
+    white: false,
+    yellow: false
+}
+
+var wiresToCut = [];
+
+//DOM REFERENCES
+var timer = document.getElementById('timer');
+var wireBox = document.getElementById('wirebox');
+var resetButton = document.querySelector('button');
